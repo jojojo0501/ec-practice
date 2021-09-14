670fea7b48a300970c9d1dda66a9427ce8afe4bd
jojojo0501 <sarukiti0418@outlook.jp>
2021-09-14 10:53:26 +0900

    注文時に使用するフォームを作成しました。

---
+++ src/main/java/jp/co/example/form/OrderForm.java
@@ -0,0 +1,84 @@
+package jp.co.example.form;
+
+public class OrderForm {
+	private String destinationName;
+	private String destinationEmail;
+	private String destinationZipcode;
+	private String destinationAddress;
+	private String destinationTel;
+	private String deliveryDate;
+	private String deliveryTime;
+	private String paymentMethod;
+
+	public String getDestinationName() {
+		return destinationName;
+	}
+
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+
+	public String getDestinationEmail() {
+		return destinationEmail;
+	}
+
+	public void setDestinationEmail(String destinationEmail) {
+		this.destinationEmail = destinationEmail;
+	}
+
+	public String getDestinationZipcode() {
+		return destinationZipcode;
+	}
+
+	public void setDestinationZipcode(String destinationZipcode) {
+		this.destinationZipcode = destinationZipcode;
+	}
+
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+
+	public String getDestinationTel() {
+		return destinationTel;
+	}
+
+	public void setDestinationTel(String destinationTel) {
+		this.destinationTel = destinationTel;
+	}
+
+	public String getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(String deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getDeliveryTime() {
+		return deliveryTime;
+	}
+
+	public void setDeliveryTime(String deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderForm [destinationName=" + destinationName + ", destinationEmail=" + destinationEmail
+				+ ", destinationZipcode=" + destinationZipcode + ", destinationAddress=" + destinationAddress
+				+ ", destinationTel=" + destinationTel + ", deliveryDate=" + deliveryDate + ", deliveryTime="
+				+ deliveryTime + ", paymentMethod=" + paymentMethod + "]";
+	}
+}
